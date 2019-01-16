e6bd14e3883061f4aa87576f126a54af2172f65b
Tripe2000 <tripe2000@gmail.com>
2019-01-16 16:19:15 -0500

    third commit

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Scanner;
 
 public class Main {
     private static final String USER_AGENT = "Mozilla/5.0";
-    private static final String POST_URL = "http://localhost:9090/SpringMVCExample/home";
-    private static final String POST_PARAMS = "userName=Pankaj";
 
     public static void main(String[] args) {
-        sendGET();
+        Scanner reader = new Scanner(System.in);
+        while(true) {
+            String inputRaw = reader.nextLine();
+            String[] input = inputRaw.split("\\s+");
+            if(input[0].equals("httpc")){
+                if(input[1].equals("get")) { sendGET(); }
+                else if(input[1].equals("post")) { sendPOST(); }
+                else { System.out.println("unknown cmd"); }
+            } else { System.out.println("unknown cmd"); }
+        }
+        reader.close();
     }
 
     private static void sendGET() {
@@ -20,11 +30,30 @@ public class Main {
             HttpURLConnection con = (HttpURLConnection) obj.openConnection();
             con.setRequestMethod("GET");
             con.setRequestProperty("User-Agent", USER_AGENT);
-            int responseCode = con.getResponseCode();
 
-            System.out.println("GET Response Code :: " + responseCode);
-            
+            System.out.println("GET Response Code :: " + con.getResponseCode());
+
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void sendPOST() {
+        try {
+            URL obj = new URL("http://www.httpbin.org/");
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("User-Agent", USER_AGENT);
+
+            con.setDoOutput(true);
+            OutputStream os = con.getOutputStream();
+            final String POST_PARAMS = "paramName=paramValue";
+            os.write(POST_PARAMS.getBytes());
+            os.flush();
+            os.close();
+
+            System.out.println("POST Response Code :: " + con.getResponseCode());
+        } catch(IOException e) {
             e.printStackTrace();
         }
     }
