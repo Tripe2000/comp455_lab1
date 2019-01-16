de58f3ee84e111423fa3d35eb8168b2308a2598a
Tripe2000 <tripe2000@gmail.com>
2019-01-16 15:52:42 -0500

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class Main {
+    private static final String USER_AGENT = "Mozilla/5.0";
+    private static final String POST_URL = "http://localhost:9090/SpringMVCExample/home";
+    private static final String POST_PARAMS = "userName=Pankaj";
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        sendGET();
+    }
+
+    private static void sendGET() {
+        try {
+            URL obj = new URL("http://www.httpbin.org/");
+
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("User-Agent", USER_AGENT);
+            int responseCode = con.getResponseCode();
+
+            System.out.println("GET Response Code :: " + responseCode);
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
